2c8788e50de579c31ee7fe35071af7f2de2510a8
Zaria Burton <zaria.burton2000@gmail.com>
2022-10-17 16:22:21 -0400

    split employee table into employee and credentials, added department table

--- FSEA_DB_Init.py
+++ FSEA_DB_Init.py
@@ -1,5 +1,6 @@
 import sqlite3
-from utils import authUtils, encryption
+from utils.encryption import *
+from utils.authUtils import *
 
 # create new db (or open if already exists)
 con = sqlite3.connect('FSEA.db')
@@ -7,62 +8,142 @@ con = sqlite3.connect('FSEA.db')
 # create cursor for statement execution
 cur = con.cursor()
 
-# drop Employee table from database if exists
+# drop Department table from database if it exists
 try:
-    con.execute('''Drop table Employee''')
+    con.execute('DROP TABLE Department')
     con.commit()
-    print("Employee table dropped")
+    print('Department table dropped')
 
 except:
-    print("Employee table does not exist.")
+    print('Department table does not exist')
+
+# drop Employee table from database if it exists
+try:
+    con.execute('''DROP TABLE Employee''')
+    con.commit()
+    print('Employee table dropped')
+
+except:
+    print('Employee table does not exist.')
+
+# drop Credentials table from database if it exists
+try:
+    con.execute('DROP TABLE Credentials')
+    con.commit()
+    print("Credentials table dropped")
+
+except:
+    print("Credentials table does not exist")
+
+# create Department table
+cur.execute('''CREATE TABLE Department(
+                depID       INT NOT NULL,
+                depName     TEXT NOT NULL,
+                description TEXT,
+                PRIMARY KEY (depID)
+                );''')
+con.commit()
 
 # create Employee table in db
-# TODO implement login attempt counter
-cur.execute('''create table Employee( 
-                empDep          TEXT CHECK(empDep IN ('SPEC-OP', 'EX-HEAD')),
+cur.execute('''CREATE TABLE Employee( 
+                empDep          INT NOT NULL,
                 empID           TEXT NOT NULL,
                 designation     TEXT CHECK(designation IN ('CP', 'ELEC-ENG', 'PLAN-GEO', 'MECH-ENG', 'CHEM')),
                 firstName       TEXT CHECK(LENGTH(firstName) <= 50) NOT NULL,
                 lastName        TEXT CHECK(LENGTH(lastName) <= 50)  NOT NULL,
-                username        TEXT NOT NULL,
-                password        TEXT NOT NULL,
-                PRIMARY KEY (empID)
+                PRIMARY KEY (empID),
+                FOREIGN KEY (empDep) REFERENCES Department(depId)
                 );''')
 
 # save changes
 con.commit()
 print('Employee table created.')
 
-data = [('SPEC-OP', '', 'CP',       'Prisca',   'Poteau', 'ppoteau_cp',     'poop'),
-        ('SPEC-OP', '', 'ELEC-ENG', 'Revy',     'Sagan',  'rsagan_eleceng', authUtils.generatePWD()),
-        ('SPEC-OP', '', 'CHEM',     'Michael',  'Lowe',   'mlowe_chem',     authUtils.generatePWD())]
+# create Credentials table
+cur.execute('''CREATE TABLE Credentials( 
+                empID           TEXT NOT NULL,
+                username        TEXT NOT NULL,
+                password        TEXT NOT NULL,
+                loginAttempts   INT DEFAULT 0,
+                PRIMARY KEY (empID),
+                CONSTRAINT employeeID
+                    FOREIGN KEY (empID)
+                    REFERENCES Employee(empID)
+                    ON DELETE CASCADE
+                );''')
+con.commit()
 
-data = [list(row) for row in data]
+# insert departments
+departments = [(1, 'ZERO'),
+              (2, 'EXEC')]
+cur.executemany('INSERT INTO Department (depID, depName) VALUES(?, ?);', departments)
+con.commit()
+print('Data inserted into Department Table')
 
-for row in data:
-    uid = authUtils.generateUID()
-    cur.execute('select empID from Employee where empID = ?', [uid])
+employees = [(1, '', 'CP', 'Prisca', 'Poteau'),
+             (1, '', 'ELEC-ENG', 'Revy',     'Sagan'),
+             (1, '', 'CHEM',     'Michael',  'Lowe')]
+
+employees = [list(row) for row in employees]
+
+# generate random empID for each employee
+for row in employees:
+    uid = generateUID()
+    print(uid)
+    cur.execute('SELECT empID FROM Employee WHERE empID = ?', [uid])
     query = cur.fetchone()
+
     while query is not None:
-        uid = authUtils.generateUID()
-        cur.execute('select empID from Employee where empID = ?', [uid])
+        uid = generateUID()
+        cur.execute('SELECT empID FROM Employee WHERE empID = ?', [uid])
         query = cur.fetchone()
-    row[1] = str(encryption.cipher.encrypt(bytes(uid, 'utf-8')).decode('utf-8'))
+
+    row[1] = uid
+    # encrypt first name
     row[3] = str(encryption.cipher.encrypt(bytes(row[3], 'utf-8')).decode('utf-8'))
+    # encrypt last name
     row[4] = str(encryption.cipher.encrypt(bytes(row[4], 'utf-8')).decode('utf-8'))
-    row[5] = str(encryption.cipher.encrypt(bytes(row[5], 'utf-8')).decode('utf-8'))
-    row[6] = str(encryption.cipher.encrypt(bytes(row[6], 'utf-8')).decode('utf-8'))
 
-data = [tuple(row) for row in data]
 
-# populate table
-cur.executemany('insert into Employee values(?,?,?,?,?,?,?);', data)
+employees = [tuple(row) for row in employees]
+
+
+# populate Employee table
+cur.executemany('INSERT INTO Employee VALUES(?,?,?,?,?);', employees)
 con.commit()
 print('Data inserted into Employee table.')
 
+# populate Credentials table
+cur.execute('SELECT empID FROM Employee;')
+query = cur.fetchall()
+# convert list of tuples to list of strings
+query = ['%s' % item for item in query]
+
+# TODO FIX USERNAME GEN
+for e in query:
+    username = generateUsername(e)
+    password = generatePWD()
+
+    cur.execute('INSERT INTO Credentials (empID, username, password) VALUES(?, ?, ?)', (e, username, password))
+
+
+print('Data inserted into Credentials table')
+
 # iterate over rows in table
-for row in cur.execute('select * from Employee;'):
+print('Department Table')
+for row in cur.execute('SELECT * FROM Department;'):
     print(row)
+print('\n')
+
+print('Employee Table')
+for row in cur.execute('SELECT * FROM Employee;'):
+    print(row)
+print('\n')
+
+print('Credentials Table')
+for row in cur.execute('SELECT * FROM Credentials;'):
+    print(row)
+
 
 con.close()
 print('Connection closed.')
