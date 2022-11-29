428c5db3c57aa6051dc16bbd016fc653340a2368
Zaria Burton <zaria.burton2000@gmail.com>
2022-11-29 11:48:01 -0500

    finished initializing all tables except for EmployeeSpecimen

--- DB_Init.py
+++ DB_Init.py
@@ -1,33 +1,52 @@
-import sqlite3
-from utils.variables import db
-from utils.databaseUtils import *
+from utils.alterDatabaseUtils import *
 import json
+
+'''
+IMPORTANT!!!!
+Run DB_Declaration.py EVERYTIME before running this script.
+'''
 
 with open('utils/data.json') as d:
     data = json.load(d)
 
 for d in data["department"]:
-    addDepartment(d['name'], desc=d['description'])
+    addDepartment(d["name"], desc=d["description"])
 
 for e in data["employee"]:
     print(e)
-    ID = addEmployee(e['firstName'], e['lastName'], e['dep'], e['designation'], e['startDate'])
+    ID = addEmployee(e["firstName"], e["lastName"], e["dep"], e["designation"], e["startDate"])
+    updateEmployeeMedical(ID, e["dob"], e["bloodtype"], e["sex"], e["weight"], e["height"], e["notes"])
 
+oID = None
+for o in data["origin"]:
+    print(o)
+    oID = addOrigin(o["name"], o["description"])
+
+mID = None
+for m in data["mission"]:
+    print(m)
+    mID = addMission(m["name"], m["description"])
+
+updateOrigin(oID, missionID=mID)
+updateMission(mID, originID=oID)
+
+for s in data["specimen"]:
+    addSpecimen(s['name'], s["acquisitionDate"], notes=s["notes"])
 
 con = sqlite3.connect(db)
 cur = con.cursor()
-# iterate over rows in table
+
+'''''''''''''''''''''''''''PRINT TABLES'''''''''''''''''''''''''''
 print('Department Table')
 for row in cur.execute('SELECT * FROM Department;'):
     print(row)
 print('\n')
 
-
 print('Employee Table')
 for row in cur.execute('SELECT * FROM Employee;'):
     print(row)
 print('\n')
-'''
+
 print('EmployeeMedical Table')
 for row in cur.execute('SELECT * FROM EmployeeMedical;'):
     print(row)
@@ -51,6 +70,18 @@ for row in cur.execute('SELECT * FROM Mission;'):
     print(row)
 print('\n')
 
+print('Specimen Table')
+for row in cur.execute('SELECT * FROM Specimen;'):
+    row = list(row)
+    print(row)
+print('\n')
+
+print('SpecimenMedical Table')
+for row in cur.execute('SELECT * FROM SpecimenMedical;'):
+    row = list(row)
+    print(row)
+print('\n')
+
 con.close()
 print('Database connection closed.')
-'''
+
