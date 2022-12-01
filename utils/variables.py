a5cac86a527575f4c6dc0805810038b39f951fdd
SpicyKneecaps <zaria.burton2000@gmail.com>
2022-11-30 23:18:50 -0500

    updated designations

--- utils/variables.py
+++ utils/variables.py
@@ -2,4 +2,4 @@
 db = 'FSEA.db'
 bloodtypes = '''('A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-', 'V-', 'V+', 'BF', 'undefined')'''
 sex = '''('m', 'f', 'inter', 'unknown','undefined')'''
-designation = ('ADMIN','CP', 'ENG', 'GEO', 'CHEM', 'SUPER', 'TECH', 'SOLDIER', 'BIO')
+designation = ('ADMIN', 'CAPTAIN', 'ENGINEER', 'GEOLOGIST', 'CHEMIST', 'SUPERVISOR', 'IT', 'SOLDIER', 'BIOLOGIST')
