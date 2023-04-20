4090d05762c2b6edaefc1f69642a61fd2273007b
Zaria Burton <zaria.burton2000@gmail.com>
2023-04-20 14:54:37 -0400

    code cleanup and bug fix

--- bulk/infoWindow.py
+++ bulk/infoWindow.py
@@ -5,7 +5,7 @@ from bulk.baseWindows import windowWithToolbar
 class employeeInfo(windowWithToolbar):
     def __init__(self, empID, parent=None):
         super().__init__(parent)
-        self.exitButton.disconnect() # removes any exit functions linked to button so it doesn't close the entire program
+        self.exitButton.disconnect() # removes any exit functions linked to button, so it doesn't close the entire program
         self.exitButton.clicked.connect(self.close)
         self.empID = empID
 
