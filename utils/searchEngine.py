6abece8b434e03ed119024f4015c6460f5af4afc
Zaria Burton <zaria.burton2000@gmail.com>
2023-04-27 16:31:19 -0400

    updates/fixes

--- utils/searchEngine.py
+++ utils/searchEngine.py
@@ -1,4 +1,6 @@
 import sqlite3
+import traceback
+
 from utils.variables import db
 
 def dict_factory(cursor, row):
@@ -17,55 +19,86 @@ def search(query):
         cur = con.cursor()
         results = [None, None, None]
 
-        cur.execute('''DELETE FROM search_results;''')
+        query = query.split()
 
-        cur.execute('''INSERT INTO search_results (type, id, firstName, lastName, description, rank)
-                        SELECT 'D', depID, NULL, depName, description, bm25(Department_fts)
-                        FROM Department_fts
-                        WHERE Department_fts MATCH ?
-                        UNION ALL
-                        SELECT 'E', empID, firstName, lastName, summary, bm25(Employee_fts)
-                        FROM Employee_fts
-                        WHERE Employee_fts MATCH ?
-                        UNION ALL
-                        SELECT 'S', specimenID, NULL, name, description, bm25(Specimen_fts)
-                        FROM Specimen_fts 
-                        WHERE Specimen_fts MATCH ?
-                        UNION ALL
-                        SELECT 'O', originID, NULL, name, description, bm25(Origin_fts)
-                        FROM Origin_fts
-                        WHERE Origin_fts MATCH ?
-                        UNION ALL
-                        SELECT 'M', missionID, NULL, name, description, bm25(Mission_fts)
-                        FROM Mission_fts
-                        WHERE Mission_fts MATCH ?
-                        ''', (query + '*', query + '*', query + '*', query + '*', query + '*'))
+        for i in range(len(query)):
+            w = query[i]
 
-        cur.execute('''SELECT type,
-                        id, 
-                        firstName, 
-                        lastName,
-                        description
-                        FROM search_results
-                        ORDER BY rank;''')
+            cur.execute('''DELETE FROM search_results;''')
 
-        results[0] = cur.fetchall()
+            cur.execute('''INSERT INTO search_results (type, id, firstName, lastName, description, rank)
+                            SELECT 'D', depID, NULL, depName, description, bm25(Department_fts)
+                            FROM Department_fts
+                            WHERE Department_fts MATCH ?
+                            UNION ALL
+                            SELECT 'E', empID, firstName, lastName, summary, bm25(Employee_fts)
+                            FROM Employee_fts
+                            WHERE Employee_fts MATCH ?
+                            UNION ALL
+                            SELECT 'S', specimenID, NULL, name, description, bm25(Specimen_fts)
+                            FROM Specimen_fts 
+                            WHERE Specimen_fts MATCH ?
+                            UNION ALL
+                            SELECT 'O', originID, NULL, name, description, bm25(Origin_fts)
+                            FROM Origin_fts
+                            WHERE Origin_fts MATCH ?
+                            UNION ALL
+                            SELECT 'M', missionID, NULL, name, description, bm25(Mission_fts)
+                            FROM Mission_fts
+                            WHERE Mission_fts MATCH ?
+                            ''', (w+ '*', w + '*', w + '*', w + '*', w + '*'))
 
-        cur.execute('''SELECT *
-                        FROM search_results
-                        ORDER BY lastName;''')
-        results[1] = cur.fetchall()
+            cur.execute('''SELECT type,
+                            id, 
+                            firstName, 
+                            lastName,
+                            description
+                            FROM search_results
+                            ORDER BY rank;''')
 
-        cur.execute('''SELECT type,
-                        id, 
-                        firstName, 
-                        lastName,
-                        description
-                        FROM search_results
-                        ORDER BY lastName DESC;''')
-        results[2] = cur.fetchall()
+
+            if i == 0:
+                results[0] = cur.fetchall()
+
+                cur.execute('''SELECT *
+                                FROM search_results
+                                ORDER BY lastName;''')
+                results[1] = cur.fetchall()
+
+                cur.execute('''SELECT type,
+                                id, 
+                                firstName, 
+                                lastName,
+                                description
+                                FROM search_results
+                                ORDER BY lastName DESC;''')
+                results[2] = cur.fetchall()
+            else:
+                results[0].append(cur.fetchall())
+
+                cur.execute('''SELECT *
+                                                FROM search_results
+                                                ORDER BY lastName;''')
+                results[1].append(cur.fetchall())
+
+                cur.execute('''SELECT type,
+                                                id, 
+                                                firstName, 
+                                                lastName,
+                                                description
+                                                FROM search_results
+                                                ORDER BY lastName DESC;''')
+                results[2].append(cur.fetchall())
+
+        # TODO: remove duplicate dictionaries from list
+        if results[0] is not None:
+            for i in range(len(results)):
+                for j in range(len(results[i])):
+                    results[i][j] = results[i][j][0]
+
 
     except Exception as e:
+        traceback.print_exc()
         print(e)
     finally:
         if con is not None:
