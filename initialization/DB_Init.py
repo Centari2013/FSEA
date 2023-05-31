960ec8d3fd77e601d163dee8c5510cb2d63a6186
SpicyKneecaps <zaria.burton2000@gmail.com>
2023-05-31 19:04:27 -0400

    more pain

--- initialization/DB_Init.py
+++ initialization/DB_Init.py
@@ -16,7 +16,6 @@ with open('complete_data.json') as d:
 def designationData():
     for dep in data["department"]:
         for des in dep["designations"]:
-            # append designationID to list of designations
             addDesignation(des[0], des[1])
 
 
@@ -41,7 +40,7 @@ def departmentData():
     # add generated depID to each department dict
     for d in data["department"]:
         d["depID"] = depID = addDepartment(name=d["name"], desc=d["description"])
-        sup = d["supervisor"]
+        sup = d.pop("supervisor")
         fn = sup["firstName"]
         ln = sup["lastName"]
         start = sup["startDate"]
@@ -59,6 +58,23 @@ def departmentData():
         updateEmployee(ID, endDate=None)
         updateEmployeeMedical(ID, birth, bt, sex, kg, height, notes)
 
+        data["employee"].append({
+            "dep": depID,
+            "designation": depSupervisorDesID,
+            "firstName": fn,
+            "lastName": ln,
+            "startDate": start,
+            "endDate": None,
+            "dob": birth,
+            "bloodtype": bt,
+            "sex": sex,
+            "weight": kg,
+            "height": height,
+            "notes": notes,
+            "summary": summ,
+            "empID": ID
+        })
+
 
 def EmployeeData():
     for e in data["employee"]:
@@ -70,18 +86,44 @@ def EmployeeData():
                     if des[1] == e["designation"]:
                         e["designation"] = des[2]
 
-        e["empID"] = ID = addEmployee(e["firstName"], e["lastName"], e["dep"], e["startDate"],
-                                      e["summary"])
+        fn = e["firstName"]
+        ln = e["lastName"]
+        start = e["startDate"]
+        summ = e["summary"]
+        birth = e.pop("dob")
+        bt = e.pop("bloodtype")
+        sex = e.pop("sex")
+        kg = e.pop("weight")
+        height = e.pop("height")
+        notes = e.pop("notes")
+        e["empID"] = ID = addEmployee(fn, ln, e["dep"], start,
+                                     summ)
         addEmployeeDesignation(ID, e["designation"])
         updateEmployeeMedical(ID, e["dob"], e["bloodtype"], e["sex"], e["weight"], e["height"], e["notes"])
+        e["medical"] = {"dob": birth,
+                        "bloodtype": bt,
+                        "sex": sex,
+                        "weight": kg,
+                        "height": height,
+                        "notes": notes}
 
         if "endDate" in e:
             updateEmployee(endDate=e["endDate"])
+        else:
+            e["endDate"] = None
 
         addEmployeeClearance(ID, e["clearance"])
 
         if e["firstName"] == "Zaria":
             updateCredentials(ID, 'test', 'test')
+
+
+def originMissionSpecimen():
+    for o in data["origin"]:
+        oID = addOrigin(o["name"], o["discoveryDate"], o["description"])
+        for m in o["missions"]:
+            mID = addMission(m["name"], m["description"], oID, m["startDate"], m["endDate"],
+                             )
 
 
 def saveData():
