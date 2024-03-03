8d6de4c6f37c950ca5f005a0890f1a3d446fdccd
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-03 15:00:48 -0500

    Update 01_schema_creation.sql

--- 01_schema_creation.sql
+++ 01_schema_creation.sql
@@ -1,59 +1,59 @@
 CREATE TABLE clearances (
-    clearanceID SERIAL PRIMARY KEY,
+    clearance_id SERIAL PRIMARY KEY,
     name TEXT,
     description TEXT
 );
 
-CREATE TABLE containmentStatuses (
-    containmentStatusID SERIAL PRIMARY KEY,
+CREATE TABLE containment_statuses (
+    containment_status_id SERIAL PRIMARY KEY,
     name TEXT,
     description TEXT
 );
 
 CREATE TABLE departments (
-    departmentID SERIAL PRIMARY KEY,
-    departmentName TEXT NOT NULL,
-    supervisorID VARCHAR(8), -- Assuming employeeID types for consistency
+    department_id SERIAL PRIMARY KEY,
+    department_name TEXT NOT NULL,
+    supervisor_id VARCHAR(8), -- Assuming employeeID types for consistency
     description TEXT DEFAULT NULL
 );
 
 CREATE TABLE designations (
-    designationID SERIAL PRIMARY KEY,
+    designation_id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     abbreviation VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE employees (
-    employeeID VARCHAR(8) PRIMARY KEY, -- Employee IDs are formatted as 'EXXXXXXX'
-    departmentID INTEGER NOT NULL,
-    firstName TEXT NOT NULL,
-    lastName TEXT NOT NULL,
-    startDate DATE NOT NULL,
-    endDate DATE DEFAULT NULL,
+    employee_id VARCHAR(8) PRIMARY KEY, -- Employee IDs are formatted as 'EXXXXXXX'
+    department_id INTEGER NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE DEFAULT NULL,
     summary TEXT DEFAULT NULL,
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (departmentID) REFERENCES departments(departmentID)
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
-CREATE TABLE employeeClearances (
-    employeeID VARCHAR(8),
-    clearanceID INTEGER,
-    PRIMARY KEY (employeeID, clearanceID),
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE,
-    FOREIGN KEY (clearanceID) REFERENCES clearances(clearanceID) ON DELETE CASCADE
+CREATE TABLE employee_clearances (
+    employee_id VARCHAR(8),
+    clearance_id INTEGER,
+    PRIMARY KEY (employee_id, clearance_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE,
+    FOREIGN KEY (clearance_id) REFERENCES clearances(clearance_id) ON DELETE CASCADE
 );
 
-CREATE TABLE employeeDesignations (
-    employeeID VARCHAR(8),
-    designationID INTEGER NOT NULL,
-    PRIMARY KEY (employeeID, designationID),
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID),
-    FOREIGN KEY (designationID) REFERENCES designations(designationID)
+CREATE TABLE employee_designations (
+    employee_id VARCHAR(8),
+    designation_id INTEGER NOT NULL,
+    PRIMARY KEY (employee_id, designation_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (designation_id) REFERENCES designations(designation_id)
 );
 
-CREATE TABLE employeeMedicalRecords (
-    employeeID VARCHAR(8) PRIMARY KEY,
+CREATE TABLE employee_medical_records (
+    employee_id VARCHAR(8) PRIMARY KEY,
     dob DATE,
     bloodtype VARCHAR(10) DEFAULT NULL CHECK (bloodtype IN ('A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-', 'V-', 'V+', 'BF', 'undefined')),
     sex VARCHAR(10) DEFAULT NULL CHECK (sex IN ('m', 'f', 'inter', 'unknown', 'undefined')),
@@ -62,116 +62,114 @@ CREATE TABLE employeeMedicalRecords (
     notes TEXT DEFAULT NULL,
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE
 );
 
 CREATE TABLE missions (
-    missionID VARCHAR(8) PRIMARY KEY, -- Mission IDs are formatted as 'MXXXXXXX'
+    mission_id VARCHAR(8) PRIMARY KEY, -- Mission IDs are formatted as 'MXXXXXXX'
     name TEXT NOT NULL DEFAULT 'NAME-PENDING',
-    startDate DATE,
-    endDate DATE,
-    commanderID VARCHAR(8),
-    supervisorID VARCHAR(8),
+    start_date DATE,
+    end_date DATE,
+    commander_id VARCHAR(8),
+    supervisor_id VARCHAR(8),
     description TEXT NOT NULL,
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (commanderID) REFERENCES employees(employeeID),
-    FOREIGN KEY (supervisorID) REFERENCES employees(employeeID) 
+    FOREIGN KEY (commander_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (supervisor_id) REFERENCES employees(employee_id) 
 );
 
 CREATE TABLE origins (
-    originID VARCHAR(8) PRIMARY KEY, -- Origin IDs are formatted as 'OXXXXXXX'
+    origin_id VARCHAR(8) PRIMARY KEY, -- Origin IDs are formatted as 'OXXXXXXX'
     name TEXT NOT NULL,
-    discoveryDate DATE NOT NULL,
+    discovery_date DATE NOT NULL,
     description TEXT NOT NULL,
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL
 );
 
-CREATE TABLE missionOrigins(
-    missionID VARCHAR(8) NOT NULL,
-    originID VARCHAR(8) NOT NULL,
-    PRIMARY KEY (missionID, originID),
-    FOREIGN KEY (originID) REFERENCES origins(originID),
-    FOREIGN KEY (missionID) REFERENCES missions(missionID) 
+CREATE TABLE mission_origins (
+    mission_id VARCHAR(8) NOT NULL,
+    origin_id VARCHAR(8) NOT NULL,
+    PRIMARY KEY (mission_id, origin_id),
+    FOREIGN KEY (origin_id) REFERENCES origins(origin_id),
+    FOREIGN KEY (mission_id) REFERENCES missions(mission_id) 
 );
 
 CREATE TABLE specimens (
-    specimenID VARCHAR(8) PRIMARY KEY, -- SpecimenIDs are formatted as 'SXXXXXXX'
+    specimen_id VARCHAR(8) PRIMARY KEY, -- SpecimenIDs are formatted as 'SXXXXXXX'
     name TEXT NOT NULL,
-    originID VARCHAR(8),
-    missionID VARCHAR(8),
-    threatLevel REAL CHECK (threatLevel >= 0 AND threatLevel <= 10),
-    acquisitionDate DATE NOT NULL,
+    origin_id VARCHAR(8),
+    mission_id VARCHAR(8),
+    threat_level REAL CHECK (threat_level >= 0 AND threat_level <= 10),
+    acquisition_date DATE NOT NULL,
     notes TEXT DEFAULT NULL,
     description TEXT DEFAULT NULL,
     created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (originID) REFERENCES origins(originID) ON DELETE CASCADE,
-    FOREIGN KEY (missionID) REFERENCES missions(missionID) ON DELETE CASCADE
+    FOREIGN KEY (origin_id) REFERENCES origins(origin_id) ON DELETE CASCADE,
+    FOREIGN KEY (mission_id) REFERENCES missions(mission_id) ON DELETE CASCADE
 );
 
-CREATE TABLE specimenContainmentStatuses (
-    specimenID VARCHAR(8) NOT NULL,
-    containmentStatusID INTEGER,
-    PRIMARY KEY (specimenID, containmentStatusID),
-    FOREIGN KEY (specimenID) REFERENCES specimens(specimenID) ON DELETE CASCADE,
-    FOREIGN KEY (containmentStatusID) REFERENCES containmentStatuses(containmentStatusID) ON DELETE CASCADE
+CREATE TABLE specimen_containment_statuses (
+    specimen_id VARCHAR(8) NOT NULL,
+    containment_status_id INTEGER,
+    PRIMARY KEY (specimen_id, containment_status_id),
+    FOREIGN KEY (specimen_id) REFERENCES specimens(specimen_id) ON DELETE CASCADE,
+    FOREIGN KEY (containment_status_id) REFERENCES containment_statuses(containment_status_id) ON DELETE CASCADE
 );
 
-CREATE TABLE specimenMedicalRecords (
-    specimenID VARCHAR(8) PRIMARY KEY ,
+CREATE TABLE specimen_medical_records (
+    specimen_id VARCHAR(8) PRIMARY KEY,
     bloodtype VARCHAR(10) DEFAULT NULL,
     sex VARCHAR(10) DEFAULT NULL,
-    kilograms REAL CHECK(kilograms > 0),
-    notes TEXT DEFAULT NULL,
-    created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (specimenID) REFERENCES specimens(specimenID) ON DELETE CASCADE
+		kilograms REAL CHECK(kilograms > 0),
+		notes TEXT DEFAULT NULL,
+		created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+		updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
+		FOREIGN KEY (specimen_id) REFERENCES specimens(specimen_id) ON DELETE CASCADE
 );
 
-CREATE TABLE specimenMissions (
-    specimenID VARCHAR(8) NOT NULL,
-    missionID VARCHAR(8) NOT NULL,
-    involvementSummary TEXT,
-    PRIMARY KEY (specimenID, missionID),
-    FOREIGN KEY (specimenID) REFERENCES specimens(specimenID) ON DELETE CASCADE,
-    FOREIGN KEY (missionID) REFERENCES missions(missionID) ON DELETE CASCADE
+CREATE TABLE specimen_missions (
+		specimen_id VARCHAR(8) NOT NULL,
+		mission_id VARCHAR(8) NOT NULL,
+		involvement_summary TEXT,
+		PRIMARY KEY (specimen_id, mission_id),
+		FOREIGN KEY (specimen_id) REFERENCES specimens(specimen_id) ON DELETE CASCADE,
+		FOREIGN KEY (mission_id) REFERENCES missions(mission_id) ON DELETE CASCADE
 );
 
 CREATE TABLE credentials (
-    employeeID VARCHAR(8) PRIMARY KEY ,
-    username DEFAULT NULL,
-    password DEFAULT NULL,
-    loginAttempts INTEGER DEFAULT 0,
-    created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE
+		employee_id VARCHAR(8) PRIMARY KEY,
+		username TEXT DEFAULT NULL,
+		password TEXT DEFAULT NULL,
+		login_attempts INTEGER DEFAULT 0,
+		created TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+		updated TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL,
+		FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE
 );
 
--- maybe add onboarding table for tracking user onboarding tasks
-
-CREATE TABLE departmentMissions (
-    departmentID INTEGER NOT NULL,
-    missionID VARCHAR(8) NOT NULL,
-    PRIMARY KEY (departmentID, missionID),
-    FOREIGN KEY (departmentID) REFERENCES departments(departmentID) ON DELETE CASCADE,
-    FOREIGN KEY (missionID) REFERENCES missions(missionID) ON DELETE CASCADE
+CREATE TABLE department_missions (
+		department_id INTEGER NOT NULL,
+		mission_id VARCHAR(8) NOT NULL,
+		PRIMARY KEY (department_id, mission_id),
+		FOREIGN KEY (department_id) REFERENCES departments(department_id) ON DELETE CASCADE,
+		FOREIGN KEY (mission_id) REFERENCES missions(mission_id) ON DELETE CASCADE
 );
 
-CREATE TABLE employeeMissions (
-    employeeID VARCHAR(8) NOT NULL,
-    missionID VARCHAR(8) NOT NULL,
-    involvementSummary TEXT DEFAULT NULL,
-    PRIMARY KEY (employeeID, missionID),
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE,
-    FOREIGN KEY (missionID) REFERENCES missions(missionID) ON DELETE CASCADE
+CREATE TABLE employee_missions (
+		employee_id VARCHAR(8) NOT NULL,
+		mission_id VARCHAR(8) NOT NULL,
+		involvement_summary TEXT DEFAULT NULL,
+		PRIMARY KEY (employee_id, mission_id),
+		FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE,
+		FOREIGN KEY (mission_id) REFERENCES missions(mission_id) ON DELETE CASCADE
 );
 
-CREATE TABLE researcherSpecimens (
-    employeeID VARCHAR(8) NOT NULL,
-    specimenID VARCHAR(8) NOT NULL,
-    PRIMARY KEY (employeeID, specimenID),
-    FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE,
-    FOREIGN KEY (specimenID) REFERENCES specimens(specimenID) ON DELETE CASCADE
+CREATE TABLE researcher_specimens (
+		employee_id VARCHAR(8) NOT NULL,
+		specimen_id VARCHAR(8) NOT NULL,
+		PRIMARY KEY (employee_id, specimen_id),
+		FOREIGN KEY (employee_id) REFERENCES employees(employee_id) ON DELETE CASCADE,
+		FOREIGN KEY (specimen_id) REFERENCES specimens(specimen_id) ON DELETE CASCADE
 );
