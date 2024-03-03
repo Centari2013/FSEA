a38bd45531a8704fe70720d57b8223e8beda4eaa
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-03 15:06:26 -0500

    Update 03_triggers.sql

--- 03_triggers.sql
+++ 03_triggers.sql
@@ -1,95 +1,64 @@
 CREATE OR REPLACE TRIGGER update_employees_modified
-    BEFORE UPDATE
-    ON employees
+    BEFORE UPDATE ON employees
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
 
-
-
-CREATE OR REPLACE TRIGGER update_employeeMedicalRecords_modified
-    BEFORE UPDATE
-    ON employeeMedicalRecords
+CREATE OR REPLACE TRIGGER update_employee_medical_records_modified
+    BEFORE UPDATE ON employee_medical_records
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
-
-
 
 CREATE OR REPLACE TRIGGER update_missions_modified
-    BEFORE UPDATE
-    ON missions
+    BEFORE UPDATE ON missions
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
-
-
 
 CREATE OR REPLACE TRIGGER update_specimens_modified
-    BEFORE UPDATE
-    ON specimens
+    BEFORE UPDATE ON specimens
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
 
-
-
-CREATE OR REPLACE TRIGGER update_specimenMedicalRecords_modified
-    BEFORE UPDATE
-    ON specimenMedicalRecords
+CREATE OR REPLACE TRIGGER update_specimen_medical_records_modified
+    BEFORE UPDATE ON specimen_medical_records
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
-
-
 
 CREATE OR REPLACE TRIGGER update_origins_modified
-    BEFORE UPDATE
-    ON origins
+    BEFORE UPDATE ON origins
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
-
-
 
 CREATE OR REPLACE TRIGGER update_credentials_modified
-    BEFORE UPDATE
-    ON credentials
+    BEFORE UPDATE ON credentials
     FOR EACH ROW
     EXECUTE FUNCTION update_modified_column();
 
-
-
 CREATE OR REPLACE TRIGGER create_employee_records
-    AFTER INSERT
-    ON employees
+    AFTER INSERT ON employees
     FOR EACH ROW
     EXECUTE FUNCTION create_employee_records();
 
-
-
 CREATE OR REPLACE TRIGGER create_specimen_medical_record
-    AFTER INSERT
-    ON specimens
+    AFTER INSERT ON specimens
     FOR EACH ROW
     EXECUTE FUNCTION create_specimen_medical_record();
 
-
-
-CREATE OR REPLACE TRIGGER insert_employeeID
-    BEFORE INSERT
-    ON employees
+CREATE OR REPLACE TRIGGER insert_employee_id
+    BEFORE INSERT ON employees
     FOR EACH ROW
-    EXECUTE FUNCTION insert_employeeID();
+    EXECUTE FUNCTION insert_employee_id();
 
-CREATE OR REPLACE TRIGGER insert_specimenID
-    BEFORE INSERT
-    ON specimens
+CREATE OR REPLACE TRIGGER insert_specimen_id
+    BEFORE INSERT ON specimens
     FOR EACH ROW
-    EXECUTE FUNCTION insert_specimenID();
+    EXECUTE FUNCTION insert_specimen_id();
 
-CREATE OR REPLACE TRIGGER insert_originID
-    BEFORE INSERT
-    ON origins
+CREATE OR REPLACE TRIGGER insert_origin_id
+    BEFORE INSERT ON origins
     FOR EACH ROW
-    EXECUTE FUNCTION insert_originID();
+    EXECUTE FUNCTION insert_origin_id();
 
-CREATE OR REPLACE TRIGGER insert_missionID
-    BEFORE INSERT
-    ON missions
+CREATE OR REPLACE TRIGGER insert_mission_id
+    BEFORE INSERT ON missions
     FOR EACH ROW
-    EXECUTE FUNCTION insert_missionID();
+    EXECUTE FUNCTION insert_mission_id();
