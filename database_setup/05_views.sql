6bf2ae2d9dde2faa37a41aed415e7c2c00417881
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-13 01:07:40 -0400

    working on views

--- database_setup/05_views.sql
+++ database_setup/05_views.sql
@@ -8,8 +8,15 @@ SELECT
     e.notes,
     e.department_id,
     d.department_name,
-    ARRAY_AGG(DISTINCT des.name) AS designations,
-    ARRAY_AGG(DISTINCT c.name) AS clearances
+    ARRAY_AGG(CONCAT(des.designation_name, ' (', des.abbreviation, ')')) AS designations,
+    ARRAY_AGG(cl.clearance_name) AS clearances
 FROM employees e
-LEFT JOIN 
+LEFT JOIN departments d ON d.department_id = e.department_id
+LEFT JOIN employee_designations ed ON ed.employee_id = e.employee_id
+LEFT JOIN designations des ON des.designation_id = ed.designation_id
+LEFT JOIN employee_clearances ecl ON ecl.employee_id = e.employee_id
+LEFT JOIN clearances cl ON cl.clearance_id = ecl.clearance_id
+GROUP BY
+    e.employee_id, d.department_id;
+
 
