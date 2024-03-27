2e5bc0a9452825e21dba914ccf03ecc3df9d528b
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-27 18:39:08 -0400

    TESTS

---
+++ 07_tests/c__entity_creation_and_deletion/ii_on_delete_cascade_tests/department_missions_mission_id.sql
@@ -0,0 +1,55 @@
+BEGIN;
+
+-- Create test data
+CREATE TEMP TABLE test_ids (
+    id SERIAL PRIMARY KEY,
+    department_id INTEGER,
+    mission_id VARCHAR(8)
+);
+
+DO $$
+DECLARE
+    v_department_id INTEGER;
+    v_mission_id VARCHAR(8);
+BEGIN
+    -- Insert a test department
+    INSERT INTO departments (department_name, description)
+    VALUES ('testing', 'testing')
+    RETURNING department_id INTO v_department_id;
+  
+    -- Insert a test mission
+    INSERT INTO missions (mission_id, mission_name, description)
+    VALUES ('testing', 'testing', 'testing')
+    RETURNING mission_id INTO v_mission_id;
+
+    -- Associate department and mission
+    INSERT INTO department_missions (department_id, mission_id)
+    VALUES (v_department_id, v_mission_id);
+
+    -- Capture test IDs
+    INSERT INTO test_ids (department_id, mission_id)
+    VALUES (v_department_id, v_mission_id);
+
+END;
+$$;
+
+-- Plan the tests
+SELECT plan(1);
+
+-- Test cascading delete on mission_id
+DELETE FROM missions WHERE mission_id = (SELECT mission_id FROM test_ids);
+
+-- Verify cascading deletion in department_missions
+SELECT ok (
+    NOT EXISTS (
+        SELECT 1
+        FROM department_missions
+        WHERE mission_id = (SELECT mission_id FROM test_ids)
+    ),
+    'Deleting a mission cascades deletion to department_missions'
+);
+
+-- Finish the tests
+SELECT * FROM finish();
+
+ROLLBACK;
