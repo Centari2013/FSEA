218811d64dadc15156cfc4605c3b13dcb7987c41
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-27 18:30:37 -0400

    tests

---
+++ 07_tests/c__entity_creation_and_deletion/ii_on_delete_cascade_tests/specimen_missions_mission_id.sql
@@ -0,0 +1,55 @@
+BEGIN;
+
+-- Create test data
+CREATE TEMP TABLE test_ids (
+    id SERIAL PRIMARY KEY,
+    specimen_id VARCHAR(8),
+    mission_id VARCHAR(8)
+);
+
+DO $$
+DECLARE
+    v_specimen_id VARCHAR(8);
+    v_mission_id VARCHAR(8);
+BEGIN
+    -- Insert a test specimen
+    INSERT INTO specimens (specimen_id, specimen_name, acquisition_date)
+    VALUES ('testing', 'testing', CURRENT_DATE)
+    RETURNING specimen_id INTO v_specimen_id;
+  
+    -- Insert a test mission
+    INSERT INTO missions (mission_id, mission_name, description)
+    VALUES ('testing', 'testing', 'testing')
+    RETURNING mission_id INTO v_mission_id;
+
+    -- Associate specimen and mission
+    INSERT INTO specimen_missions (specimen_id, mission_id, involvement_summary)
+    VALUES (v_specimen_id, v_mission_id, 'testing');
+
+    -- Capture test IDs
+    INSERT INTO test_ids (specimen_id, mission_id)
+    VALUES (v_specimen_id, v_mission_id);
+
+END;
+$$;
+
+-- Plan the tests
+SELECT plan(1);
+
+-- Test cascading delete on mission_id
+DELETE FROM missions WHERE mission_id = (SELECT mission_id FROM test_ids);
+
+-- Verify cascading deletion in specimen_missions
+SELECT ok (
+    NOT EXISTS (
+        SELECT 1
+        FROM specimen_missions
+        WHERE mission_id = (SELECT mission_id FROM test_ids)
+    ),
+    'Deleting a mission cascades deletion to specimen_missions'
+);
+
+-- Finish the tests
+SELECT * FROM finish();
+
+ROLLBACK;
