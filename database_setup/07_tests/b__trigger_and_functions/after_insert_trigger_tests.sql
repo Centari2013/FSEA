29e1cd40a80d9b68b291e3cb05fbefb6a4c1af9b
Zaria Burton <zaria.burton2000@gmail.com>
2024-03-27 17:21:01 -0400

    critical test fix

--- database_setup/07_tests/b__trigger_and_functions/after_insert_trigger_tests.sql
+++ database_setup/07_tests/b__trigger_and_functions/after_insert_trigger_tests.sql
@@ -1,16 +1,37 @@
-SELECT plan(7);
+-- insert test data
+CREATE TEMP TABLE test_ids (
+    id SERIAL,
+    e_id VARCHAR(8),
+    s_id VARCHAR(8)
+);
 
--- data must first be committed
-SELECT lives_ok(
-    'INSERT INTO employees (first_name, last_name, department_id, start_date)
-    VALUES (''testing'', ''testing'', 1, CURRENT_DATE)',
-    'Insert of test employee successful.'
-    );
+DO $$
+DECLARE
+    v_employee_id VARCHAR(8);
+    v_specimen_id VARCHAR(8);
+BEGIN
+  
+    INSERT INTO employees (first_name, last_name, department_id, start_date)
+    VALUES ('testing', 'testing', 1, CURRENT_DATE)
+    RETURNING employee_id INTO v_employee_id;
+  
+  
+    INSERT INTO test_ids (e_id)
+    VALUES (v_employee_id);
 
-SELECT lives_ok ('INSERT INTO specimens (specimen_name, acquisition_date)
-    VALUES (''testing'', CURRENT_DATE)',
-    'Insert of test specimen successful.');
+    INSERT INTO specimens (specimen_name, acquisition_date)
+    VALUES ('testing', CURRENT_DATE)
+    RETURNING specimen_id INTO v_specimen_id;
 
+    UPDATE test_ids
+    SET s_id = v_specimen_id
+    WHERE id = 1;
+
+END;
+$$;
+
+
+SELECT plan(3);
 
 
 -- the following tests are structured as such due to auto-generated ids
@@ -22,7 +43,7 @@ SELECT is(
         SELECT COUNT(*) 
         FROM employees e
         JOIN employee_medical_records emr ON e.employee_id = emr.employee_id
-        WHERE e.first_name = 'testing'
+        WHERE e.employee_id = (SELECT e_id FROM test_ids)
     ) AS integer),
     1,
     'create_employee_records trigger correctly inserts into employee_medical_records'
@@ -33,7 +54,7 @@ SELECT is(
         SELECT COUNT(*) 
         FROM employees e
         JOIN credentials c ON e.employee_id = c.employee_id
-        WHERE e.first_name = 'testing'
+        WHERE e.employee_id = (SELECT e_id FROM test_ids)
     ) AS integer),
     1,
     'create_employee_records trigger correctly inserts into credentials'
@@ -45,19 +66,16 @@ SELECT is(
         SELECT COUNT(*) 
         FROM specimens s
         JOIN specimen_medical_records smr ON s.specimen_id = smr.specimen_id
-        WHERE s.specimen_name = 'testing'
+        WHERE s.specimen_id = (SELECT s_id FROM test_ids)
     ) AS integer),
     1,
     'create_specimen_medical_record trigger correctly inserts into specimen_medical_records'
 );
 
+SELECT * FROM finish();
 
 
 -- test data removal
-SELECT lives_ok (
-    'DELETE FROM employees WHERE first_name = ''testing''',
-    'Deletion of test employee successful.');
-SELECT lives_ok (
-    'DELETE FROM specimens WHERE specimen_name = ''testing''',
-    'Deletion of test specimen successful.');
-SELECT * FROM finish();
+DELETE FROM employees WHERE employee_id = (SELECT e_id FROM test_ids);
+DELETE FROM specimens WHERE specimen_id = (SELECT s_id FROM test_ids);
+
