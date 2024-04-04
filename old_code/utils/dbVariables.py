0c97f39253640d79c8865d1650bb934594015a2b
Zaria Burton <zaria.burton2000@gmail.com>
2024-04-03 20:23:35 -0400

    removed old code

--- old_code/utils/dbVariables.py
+++
@@ -1,8 +0,0 @@
-bloodtypes = ('A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-', 'V-', 'V+', 'BF', 'undefined')
-sex = ('m', 'f', 'inter', 'unknown','undefined')
-designation = ('ADMIN', 'CAPTAIN', 'ENGINEER', 'GEOLOGIST', 'CHEMIST', 'SUPERVISOR', 'IT', 'SOLDIER', 'BIOLOGIST')
-employeeType = 'E'
-specimenType = 'S'
-originType = 'O'
-missionType = 'M'
-departmentType = 'D'
