bedceae74462669097442d0f8036cb0a6c4f0d67
Zaria Burton <zaria.burton2000@gmail.com>
2025-01-29 05:58:44 -0500

    reintegrated search

--- frontend/src/components/Dashboard/MenuButton.vue
+++ frontend/src/components/Dashboard/MenuButton.vue
@@ -1,6 +1,6 @@
 <template>
-  <li class="h-1/12 flex pl-5 items-center hover:bg-gray-700 text-white hover:text-blue-500">
-    {{ name }}
+  <li class="h-1/12 flex pl-5 items-center hover:bg-gray-700 text-button hover:text-text-hover">
+    <strong>{{ name }}</strong>
   </li>
 </template>
 
--- frontend/src/components/Dashboard/ResultsContainer.vue
+++ frontend/src/components/Dashboard/ResultsContainer.vue
@@ -1,45 +1,42 @@
 <template>
-  <div id="CardsContainer" class="flex flex-col justify-center pb-40">
+  <div id="CardsContainer" class="flex flex-col items-center pb-40 pt-5 space-y-4">
     <template v-for="obj in results">
-      <div class="">
-        <a  class="">
-            <div class="card h-50" data-type="employee" >
-                <div class="card-body">
-                    <h5 v-if="obj.entityType === 'E'" class="card-title">{{ `${obj.first_name} ${obj.last_name}`}} </h5>
-                    <h5 v-if="obj.entityType === 'S'" class="card-title">{{ obj.specimen_name }}</h5>
-                    <h5 v-if="obj.entityType === 'M'" class="card-title"> {{obj.mission_name}}</h5>
-                    <h5 v-if="obj.entityType === 'O'" class="card-title"> {{obj.origin_name}}</h5>
+            <a class="card h-min-[17vh] border-1 rounded-md w-11/12 flex flex-col justify-center hover:shadow-md hover:cursor-pointer hover:scale-103 shadow-black" :data-type="obj.entityType" :class="getThreatCategory(obj.threat_level)">
+                <div class="card-body flex flex-col items-start">
+                    <h5 v-if="obj.entityType === 'E'" class="card-title text-xl font-bold">{{ `${obj.first_name} ${obj.last_name}`}}</h5>
+                    <h5 v-if="obj.entityType === 'S'" class="card-title text-xl font-bold">{{ obj.specimen_name }}</h5>
+                    <h5 v-if="obj.entityType === 'M'" class="card-title text-xl font-bold">{{obj.mission_name}}</h5>
+                    <h5 v-if="obj.entityType === 'O'" class="card-title text-xl font-bold">{{obj.origin_name}}</h5>
 
-                    <p class="card-text">
-                      <template v-if="obj.entityType === 'E'">
-                        <strong>Employee ID:</strong>{{ obj.employee_id }}<br>
-                        <strong>Department:</strong> {{ obj.department }}<br>
-                        <strong>Designations:</strong> {{ getDesignationString(obj) }}
-                      </template>
+                    <p class="card-text flex flex-col items-start text-left">
+                        <template v-if="obj.entityType === 'E'">
+                        <div><strong>Employee ID:</strong> {{ obj.employee_id }}</div>
+                        <div><strong>Department:</strong> {{ obj.department }}</div>
+                        <div><strong>Designations:</strong> {{ getDesignationString(obj) }}</div>
+                        </template>
 
-                      <template v-if="obj.entityType === 'S'">
-                        <strong>Specimen ID:</strong>{{ obj.specimen_id }}<br>
-                        <strong>Threat Level:</strong> {{obj.threat_level}}<br>
-                        <strong>Acquisition Date:</strong> {{obj.acquisition_date}}<br>
-                      </template>
+                        <template v-if="obj.entityType === 'S'">
+                        <div><strong>Specimen ID:</strong> {{ obj.specimen_id }}</div>
+                        <div><strong>Threat Level:</strong> {{ obj.threat_level }}</div>
+                        <div><strong>Acquisition Date:</strong> {{ obj.acquisition_date }}</div>
+                        </template>
 
-                      <template v-if="obj.entityType === 'M'">
-                        <strong>Mission ID:</strong> {{obj.mission_id}}<br>
-                        <strong>Start Date:</strong> {{obj.start_date}}<br>
-                        <strong>End Date:</strong> {{obj.end_date}}<br>
+                        <template v-if="obj.entityType === 'M'">
+                        <div><strong>Mission ID:</strong> {{ obj.mission_id }}</div>
+                        <div><strong>Start Date:</strong> {{ obj.start_date }}</div>
+                        <div><strong>End Date:</strong> {{ obj.end_date }}</div>
+                        <div>{{ obj.description || "No description available." }}</div>
+                        </template>
+
+                        <template v-if="obj.entityType === 'O'">
+                        <div><strong>Origin ID:</strong> {{ obj.origin_id }}</div>
+                        <div><strong>Discovery Date:</strong> {{ obj.discovery_date }}</div>
                         {{obj.description || "No description available."}}
-                      </template>
-
-                      <template v-if="obj.entityType === 'O'">
-                        <strong>Origin ID:</strong> {{obj.origin_id}}<br>
-                        <strong>Discovery Date:</strong> {{obj.discovery_date}}<br>
-                        {{obj.description || "No description available."}}
-                      </template>
+                        </template>
                     </p>
                 </div>
-            </div>
-        </a>
-    </div>
+              </a>
+       
     </template>
   </div>
 </template>
@@ -54,6 +51,12 @@ export default {
   methods: {
     getDesignationString(employee){
       return employee.designations ? employee.designations.map(d => d.abbreviation).join(', '): ""
+    },
+    getThreatCategory(threatLevel) {
+      if (threatLevel <= 3) return "border-l-green-500";
+      if (threatLevel <= 6) return "border-l-yellow-500";
+      if (threatLevel > 6) return "border-l-red-500";
+      return '';
     }
   }
 }
