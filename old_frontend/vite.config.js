c17bc38168b5a4d41677b23f69a78e170102e9a7
Zaria Burton <zaria.burton2000@gmail.com>
2025-01-31 00:15:24 -0500

    added svgloader for later

--- old_frontend/vite.config.js
+++ old_frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import { injectEmployeeIdMiddleware } from './middleware/injectEmployeeIdMiddleware';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 
 
 const apiKey = process.env.VITE_API_ENDPOINT;
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    svgLoader(),
   ],
   define: {
     __API_KEY__: JSON.stringify(apiKey),
