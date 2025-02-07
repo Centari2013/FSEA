1a5d0bdda44ab7201dd71dd90b23597c8fb44a42
Zaria Burton <zaria.burton2000@gmail.com>
2025-02-07 15:14:58 -0500

    removed unnecesary files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,10 +19,5 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true, // This allows access from external IPs, useful for Docker
-  
-    setupMiddlewares: (middlewares, { app }) => {
-      app.use(injectEmployeeIdMiddleware);
-      return middlewares;
-    },
   },
 })
