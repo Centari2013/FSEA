d2cbc493ad0cc88f997748fc45b651a1491506ea
Zaria Burton <zaria.burton2000@gmail.com>
2025-02-11 23:51:55 -0500

    sending empid with every request

--- src/scripts/api_access/apollo_client.js
+++ src/scripts/api_access/apollo_client.js
@@ -3,4 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
     uri: __API_URL__,
     cache: new InMemoryCache(),
+    headers: {
+        "employee_id": localStorage.getItem("employee_id") || "default-id"
+    }
 })
